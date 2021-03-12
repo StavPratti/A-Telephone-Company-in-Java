7ff7b2cd647cb15c176d3d1226bcaa8081804304
Stavroula_Pratti <56409214+StavPratti@users.noreply.github.com>
2021-03-13 01:50:13 +0200

    Add files via upload

---
+++ contracts/SimpleContract.java
@@ -0,0 +1,27 @@
+package gr.hua.it219151.contracts;
+
+
+//this is a simple contract
+public class SimpleContract {
+    private String contractID;
+    private String type;
+
+
+    public String getContractID() {
+        return contractID;
+    }
+
+    public void setContractID(String contractID) {
+        this.contractID = contractID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+}
